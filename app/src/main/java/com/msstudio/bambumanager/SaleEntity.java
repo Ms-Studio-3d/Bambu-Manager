4c732536da11ac022a2b91cf10f1bcf3b949caac
Ms-Studio-3d <mohsalah_12@yahoo.com>
2026-04-27 11:11:30 +0300

    Update SaleEntity.java

--- app/src/main/java/com/msstudio/bambumanager/SaleEntity.java
+++ app/src/main/java/com/msstudio/bambumanager/SaleEntity.java
@@ -6,7 +6,6 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = "sales")
 public class SaleEntity {
-
     @PrimaryKey
     public long id;
 
@@ -25,15 +24,63 @@ public class SaleEntity {
     public double weight;
     public double waste;
 
-    public SaleEntity(long id,
-                      @NonNull String date,
-                      @NonNull String client,
-                      @NonNull String model,
-                      double sale,
-                      double profit,
-                      double hours,
-                      double weight,
-                      double waste) {
+    @NonNull
+    public String printerName;
+
+    public double machineRate;
+    public double machineCost;
+
+    @NonNull
+    public String materialsJson;
+
+    public double materialCost;
+    public double averageGramCost;
+    public double wasteCost;
+
+    public double manualMinutes;
+    public double manualRate;
+    public double manualCost;
+
+    public double packagingCost;
+    public double totalCost;
+
+    public double profitPercent;
+    public double priceBeforeDiscount;
+    public double discount;
+    public double priceAfterDiscount;
+
+    public double finalPrice;
+    public double netProfit;
+
+    public SaleEntity(
+            long id,
+            @NonNull String date,
+            @NonNull String client,
+            @NonNull String model,
+            double sale,
+            double profit,
+            double hours,
+            double weight,
+            double waste,
+            @NonNull String printerName,
+            double machineRate,
+            double machineCost,
+            @NonNull String materialsJson,
+            double materialCost,
+            double averageGramCost,
+            double wasteCost,
+            double manualMinutes,
+            double manualRate,
+            double manualCost,
+            double packagingCost,
+            double totalCost,
+            double profitPercent,
+            double priceBeforeDiscount,
+            double discount,
+            double priceAfterDiscount,
+            double finalPrice,
+            double netProfit
+    ) {
         this.id = id;
         this.date = date;
         this.client = client;
@@ -43,5 +90,24 @@ public class SaleEntity {
         this.hours = hours;
         this.weight = weight;
         this.waste = waste;
+
+        this.printerName = printerName;
+        this.machineRate = machineRate;
+        this.machineCost = machineCost;
+        this.materialsJson = materialsJson;
+        this.materialCost = materialCost;
+        this.averageGramCost = averageGramCost;
+        this.wasteCost = wasteCost;
+        this.manualMinutes = manualMinutes;
+        this.manualRate = manualRate;
+        this.manualCost = manualCost;
+        this.packagingCost = packagingCost;
+        this.totalCost = totalCost;
+        this.profitPercent = profitPercent;
+        this.priceBeforeDiscount = priceBeforeDiscount;
+        this.discount = discount;
+        this.priceAfterDiscount = priceAfterDiscount;
+        this.finalPrice = finalPrice;
+        this.netProfit = netProfit;
     }
 }
